a3fb6817588aff98312490db41b161e24e7512ad
gavrielocraft-ai <your@email.com>
2026-06-28 23:31:03 +0800

    fix page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-"use client";
-import App from "@/components/ui/3d-folder";
-
-export default function DemoOne() {
-  return <App />;
-}
+"use client"; 
+import App from "@/components/ui/3d-folder"; 
+ 
+export default function DemoOne() { 
+  return <App />; 
+} 
