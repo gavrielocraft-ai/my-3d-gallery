9a371df2f7a6c7ce3e948c14e90f3cc5a2f592d5
gavrielocraft-ai <your@email.com>
2026-06-28 21:08:36 +0800

    update page to 3d folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-"use client"
-
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
-import { ParticleSphere } from "@/components/ui/3d-orbit-gallery"
+import Component from "@/components/ui/3d-folder";
 
 export default function DemoOne() {
-  return (
-    <div className="w-full h-screen bg-black relative">
-      <Canvas camera={{ position: [-10, 1.5, 10], fov: 50 }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} intensity={1} />
-        <ParticleSphere />
-        <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-      </Canvas>
-    </div>
-  )
+  return <Component />;
 }
